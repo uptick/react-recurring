b69777f5d46313b2148b2d0509f536b9f7cd9e4f
Luke Hodkinson <furious.luke@gmail.com>
2016-05-03 16:38:27 +1000

    A little more cleaning up.

--- example/index.jsx
+++ example/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import RecurrenceInput from '../src'
+import RecurrenceInput, { OffsetInput } from 'react-recurring'
 
-let elem = document.getElementById( 'mount' );
-ReactDOM.render( <RecurrenceInput onChange={ v => console.log( v ) } />, elem );
+ReactDOM.render( <RecurrenceInput onChange={ v => console.log( v ) } />, document.getElementById( 'mount' ) );
+ReactDOM.render( <OffsetInput />, document.getElementById( 'offset-mount' ) );
